aa6b0c8c512a658f7e761ba2684328621f73af22
Olly Mills <kinglslbob@gmail.com>
2021-02-22 14:47:50 +0000

    a "gun" that exists

---
+++ doom_the_hedge/Assets/Scripts/Weapons/bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float damage;
+    public float lifetime;
+    public string tag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if(lifetime<0)
+        {
+            kill();
+        }
+    }
+
+    void kill()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag(tag))
+        {
+            Destroy(collision.collider.gameObject);
+            kill();
+        }
+    }
+}
---
+++ doom_the_hedge/Assets/Scripts/Weapons/gun.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject aimTarget;
+    public float fireRate;
+    float curFireRate;
+    public int maxAmmo;
+    public int currAmmo;
+    public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curFireRate += Time.deltaTime;
+        //can fire
+        if(curFireRate > fireRate)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                fire();
+            }
+        }
+    }
+
+
+    void fire()
+    {
+        var shot = Instantiate(projectile, transform.position, Quaternion.identity, transform);
+        var rb = shot.GetComponent<Rigidbody>();
+        var bullet = shot.GetComponent<bullet>();
+
+        // set direction 
+        Vector3 dir = aimTarget.transform.position - transform.position;
+        rb.velocity = dir * 10;
+        //set shot details
+        bullet.damage = damage;
+        bullet.lifetime = 5;
+        bullet.tag = "enemy";
+    }
+}
